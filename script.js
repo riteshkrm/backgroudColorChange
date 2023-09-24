be878df6210e872a6c96bc6dcab2f362ad74549f
Your Name <you@example.com>
2023-09-24 22:32:22 +0530

    complete

---
+++ script.js
@@ -0,0 +1,29 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button')
+
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.id === 'grey'){
+            body.style.backgroundColor = e.target.id;
+        }
+        if(e.target.id === 'white'){
+            body.style.backgroundColor = e.target.id;
+        }
+
+        if(e.target.id === 'blue'){
+            body.style.backgroundColor = e.target.id;
+        }
+
+        if(e.target.id === 'yellow'){
+            body.style.backgroundColor = e.target.id;
+        }
+
+        if(e.target.id === 'pink'){
+            body.style.backgroundColor = e.target.id;
+        }
+
+
+
+    })
+
+} )
